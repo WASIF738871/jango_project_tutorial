16462d29e0cf1b7b2e4c90a4dc08d610e51502e4
WASIF738871 <erwasifmohd786@gmail.com>
2023-08-01 10:40:26 +0530

    CRUD operations

--- home/models.py
+++ home/models.py
@@ -11,3 +11,12 @@ class Product(models.Model):
     # def __str__(self):
     #     return self.name
 
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    # Add more fields as needed (e.g., mileage, price, etc.)
+
+    def __str__(self):
+        return f"{self.year} {self.make} {self.model}"
